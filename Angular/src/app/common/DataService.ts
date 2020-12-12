63cc2d0ce4b52badd26336e5fcd38c46a1dcb638
Unknown <m.pochmann@icloud.com>
2020-12-12 21:03:42 +0100

    Schichtplan Title mit Tagen

--- Angular/src/app/common/DataService.ts
+++ Angular/src/app/common/DataService.ts
@@ -9,11 +9,10 @@ import { retry, catchError } from 'rxjs/operators';
 })
 export class DataService {
 
-<<<<<<< HEAD
-  private REST_API_SERVER = 'http://192.168.178.20:8080';
-=======
+
+
   private REST_API_SERVER = 'http://localhost:8080';
->>>>>>> SpringFeatures
+
 
   constructor(private httpClient: HttpClient) { }
 
