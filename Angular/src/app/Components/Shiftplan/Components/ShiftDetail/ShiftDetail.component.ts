138c7756ca4e0419493c8274a17dbca7d33ebf7a
Martin Möllenhoff <martin.knuth89@gmail.com>
2020-12-14 19:04:08 +0100

    isCanceled eingebunden

--- Angular/src/app/Components/Shiftplan/Components/ShiftDetail/ShiftDetail.component.ts
+++ Angular/src/app/Components/Shiftplan/Components/ShiftDetail/ShiftDetail.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Shift} from '../../Models/Shift';
+import {DataService} from '../../../../Common/DataService';
+import {SharedService} from '../../../../Common/SharedService';
 
 @Component({
   selector: 'app-shift-detail',
@@ -8,8 +10,16 @@ import {Shift} from '../../Models/Shift';
 })
 export class ShiftDetailComponent implements OnInit {
   @Input()  shift: Shift;
-  constructor() { }
+  constructor(private api: DataService, public globalVariables: SharedService) { }
 
   ngOnInit(): void {
+  }
+
+  cancel(): void{
+    this.api.sendGetRequest('/shiftSetIsCanceled?shiftId=' + this.shift.id + '&localId='
+      + this.globalVariables.getLocalID() + '&employeeName='
+      + this.globalVariables.getNickName() + '&isCanceled=' + !this.shift.canceled).subscribe((data: Shift) => {
+      this.shift = data;
+    });
   }
 }
