f701bf150fc4f109b51965ae39bce99f1b4f30bf
Martin Möllenhoff <martin.knuth89@gmail.com>
2020-12-19 19:29:54 +0100

    Registrier Funktion fertig

--- Angular/src/app/Components/General/SignUp/SignUp.component.ts
+++ Angular/src/app/Components/General/SignUp/SignUp.component.ts
@@ -1,10 +1,17 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import {Table} from '../Models/Table';
+import {Component, OnInit} from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup, FormGroupDirective, NgForm,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
 import {DataService} from '../../../Common/DataService';
 import {SharedService} from '../../../Common/SharedService';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
-import {UserResponse} from "../Models/UserResponse";
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {UserResponse} from '../Models/UserResponse';
+import {ErrorStateMatcher} from "@angular/material/core";
 
 @Component({
   selector: 'app-sign-up',
@@ -13,10 +20,12 @@ import {UserResponse} from "../Models/UserResponse";
 })
 export class SignUpComponent implements OnInit {
   form: FormGroup = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-  });
-
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [ Validators.required ]),
+    confirmpassword: new FormControl('', [Validators.required])
+  }, { validators: passwordMatchValidator});
+  hide = true;
+  rehide = true;
   constructor(private api: DataService, public globalVariables: SharedService, public dialog: MatDialog,
               public dialogRef: MatDialogRef<SignUpComponent>) { }
 
@@ -24,21 +33,43 @@ export class SignUpComponent implements OnInit {
   }
 
   checkregistration(): void{
-    this.api.sendGetRequest('/registerUser?username=' +
-      this.form.get('username').value + '&password=' +
-      this.form.get('password').value)
-      .subscribe((data: UserResponse) => {
-        alert(data.message);
-        if (data.success)
-        {
-          this.globalVariables.setNickName(this.form.get('username').value);
-          this.globalVariables.setUser(data);
-          this.dialogRef.close(true);
-        }
-        else {
-          this.dialogRef.close(false);
-        }
-      });
+    if (this.form.valid){
+      this.api.sendGetRequest('/registerUser?username=' +
+        this.form.get('username').value + '&password=' +
+        this.form.get('password').value)
+        .subscribe((data: UserResponse) => {
+          alert(data.message);
+          if (data.success)
+          {
+            this.globalVariables.setNickName(this.form.get('username').value);
+            this.globalVariables.setUser(data);
+            this.dialogRef.close(true);
+          }
+          else {
+            this.dialogRef.close(false);
+          }
+        });
+    }
   }
 
+  get password(): AbstractControl { return this.form.get('password'); }
+  get confirmpassword(): AbstractControl { return this.form.get('confirmpassword'); }
+
+  onPasswordInput(): void {
+    if (this.form.hasError('passwordMismatch')) {
+      this.confirmpassword.setErrors([{passwordMismatch: true}]);
+    }
+    else {
+      this.confirmpassword.setErrors(null);
+    }
+  }
 }
+export const passwordMatchValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null => {
+  if (formGroup.get('password').value === formGroup.get('confirmpassword').value) {
+    return null;
+  }
+  else {
+    return { passwordMismatch: true};
+  }
+};
+
