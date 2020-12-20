3e0667e1ccd26cdb24644b9cbb13b88dee289212
Unknown <m.pochmann@icloud.com>
2020-12-20 15:58:55 +0100

    init unit tests

---
+++ SpringDienstplanverwaltung/src/test/java/ffhs/students/projects/dienstplanverwaltung/database/IDatabaseManagerTest.java
@@ -0,0 +1,119 @@
+package ffhs.students.projects.dienstplanverwaltung.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IDatabaseManagerTest {
+    /* Bedingung für den Test ist ein Lokal mit der id = 0
+    dieses muss (falls nicht vorhanden) "per Hand" angelegt werden
+     */
+
+
+    @Test
+    void getLocalById() {
+
+    }
+
+    @Test
+    void getAllLocals() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+
+
+    @Test
+    void getShift() {
+    }
+
+    @Test
+    void getShifts() {
+    }
+
+    @Test
+    void getShiftTemplates() {
+    }
+
+    @Test
+    void getShiftTemplateById() {
+    }
+
+    @Test
+    void getEmployeeForName() {
+    }
+
+    @Test
+    void getGrantedLocalsForUser() {
+    }
+
+    @Test
+    void getUserForNicknameAndPassword() {
+    }
+
+    @Test
+    void getSlotForSlotIdAndShift() {
+    }
+
+    @Test
+    void getOwnedLocalsForUser() {
+    }
+
+    @Test
+    void createOrUpdateEmployee() {
+    }
+
+    @Test
+    void createShift() {
+    }
+
+    @Test
+    void createEmployeeInLocal() {
+    }
+
+    @Test
+    void createUserIfNotExist() {
+    }
+
+    @Test
+    void createOrUpdateShiftTemplate() {
+    }
+
+    @Test
+    void requestNewLocal() {
+    }
+
+    @Test
+    void assignEmployeeToSlot() {
+    }
+
+    @Test
+    void applyEmployeeToSlot() {
+    }
+
+    @Test
+    void addServiceRole() {
+    }
+
+    @Test
+    void updateServiceRole() {
+    }
+
+    @Test
+    void updateLocal() {
+    }
+
+    @Test
+    void grantLocal() {
+    }
+
+    @Test
+    void localSetState() {
+    }
+
+    @Test
+    void setIsCanceledForShift() {
+    }
+}
